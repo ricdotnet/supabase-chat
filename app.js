90bfb78df066e8b4094b5bca36898620816dc270
Ricardo Rocha <me@rrocha.uk>
2021-07-25 20:09:12 +0100

    :rocket: new chat app...

---
+++ app.js
@@ -0,0 +1,62 @@
+const client = supabase.createClient("https://fpefdkqiasnojgxisabk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzIzMjk0NSwiZXhwIjoxOTQyODA4OTQ1fQ.8jp3hypJTP-jqgOPaAyzZ5y8NRlLBh8aE553lj-RY4w")
+client
+  .from('messages')
+  .on('INSERT', payload => {
+    updateChats(payload)
+  })
+  .subscribe()
+
+let messagesList = document.getElementById('messages')
+
+async function sendChat() {
+  let username = document.getElementById('username').value;
+  let message = document.getElementById('message').value;
+
+  if(!username)
+    return alert('enter a username to send messages...')
+
+  if(!message)
+    return alert('enter a message...')
+
+  const { data, error } = await client
+    .from('messages')
+    .insert([
+      { user: username, message: message },
+    ])
+
+  if(error) {
+    console.log('there was an error')
+    return;
+  }
+
+  message.value = ''
+}
+
+function updateChats(data) {
+
+  if(messagesList.length === 0)
+    messagesList.textContent = ''
+
+  let node = document.createElement('li')
+  let text = document.createTextNode(data.new.user + ' -> ' + data.new.message)
+  node.appendChild(text)
+  messagesList.prepend(node)
+}
+
+window.addEventListener('load', async (event) => {
+  let { data: messages, error } = await client
+    .from('messages')
+    .select('*')
+    .order('id', { ascending: false })
+
+  if(messages.length < 1) {
+    messagesList.textContent = 'no messages...'
+  }
+
+  messages.map(message => {
+    let node = document.createElement('li')
+    let text = document.createTextNode(message.user + ' -> ' + message.message)
+    node.appendChild(text)
+    messagesList.appendChild(node)
+  })
+})
